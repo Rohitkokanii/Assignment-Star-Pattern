a364742ac636844606a2bd92863ee34c4f14af9a
Rohit Kokani <148274186+Rohitkokanii@users.noreply.github.com>
2025-03-01 16:55:31 +0530

    add

--- Q16.c
+++ Q16.c
@@ -4,10 +4,14 @@
 
 int main() {
 
-	for (int i = 1; i <= 3; i++) {
-		if (i == 2 || i == 3) {
+	for (int i = 1; i <= 5; i++) {
+		if (i == 2 || i == 4) {
 			printf("   ");
 		}
+		else if (i == 3) {
+			printf("      ");
+		}
+		
 		for (int j = 1; j <= 3; j++) {	
 				printf(" * ");
 		}
